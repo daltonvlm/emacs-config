f7fefc2cf253e811d3f98804b6c6ca5750047311
Dalton Martins <daltonvlm@gmail.com>
2026-06-21 04:04:09 -0300

    feat(cape): improve CAPF ordering and add prefix map binding
    Prioritize Elisp-specific completion functions, add `cape-history`,
    and bind `cape-prefix-map` to `C-c p`.

--- .emacs.d/lisp/packages/pkg-cape.el
+++ .emacs.d/lisp/packages/pkg-cape.el
@@ -6,12 +6,15 @@
 ;;; Code:
 
 (use-package cape
+  :bind
+  ("C-c p" . cape-prefix-map)
   :init
-  (add-to-list 'completion-at-point-functions #'cape-dabbrev t)
+  (add-to-list 'completion-at-point-functions #'cape-elisp-symbol t)
+  (add-to-list 'completion-at-point-functions #'cape-elisp-block t)
   (add-to-list 'completion-at-point-functions #'cape-file t)
   (add-to-list 'completion-at-point-functions #'cape-keyword t)
-  (add-to-list 'completion-at-point-functions #'cape-elisp-block t)
-  (add-to-list 'completion-at-point-functions #'cape-elisp-symbol t))
+  (add-to-list 'completion-at-point-functions #'cape-history t)
+  (add-to-list 'completion-at-point-functions #'cape-dabbrev t))
 
 (provide 'pkg-cape)
 
